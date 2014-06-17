0e268b5ea3eafa80ae378a456fdff61d38261ae3
Theesh Mohan <ratheeshedu@gmail.com>
2014-06-17 20:50:34 +0400

    New test project: ImageSubtractionCameraPreview

---
+++ tutorial-1-camerapreview/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,145 @@
+obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ jni/DetectionBasedTracker_jni.cpp jni/DetectionBasedTracker_jni.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/jni.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ ../../sdk/native/jni/include/opencv2/core/core.hpp \
+ ../../sdk/native/jni/include/opencv2/core/types_c.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/assert.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/string.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/malloc.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/alloca.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/strings.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/stdint.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/memory.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/math.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/limits.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ ../../sdk/native/jni/include/opencv2/core/version.hpp \
+ C:/Dev/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm \
+ C:/Dev/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath \
+ C:/Dev/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef \
+ C:/Dev/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/complex \
+ C:/Dev/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/map \
+ C:/Dev/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/vector \
+ ../../sdk/native/jni/include/opencv2/core/operations.hpp \
+ C:/Dev/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/limits \
+ ../../sdk/native/jni/include/opencv2/core/mat.hpp \
+ ../../sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp \
+ ../../sdk/native/jni/include/opencv2/objdetect/objdetect.hpp \
+ C:/Dev/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/deque \
+ C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/android/log.h
+
+jni/DetectionBasedTracker_jni.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/jni.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+../../sdk/native/jni/include/opencv2/core/core.hpp:
+
+../../sdk/native/jni/include/opencv2/core/types_c.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/assert.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/string.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/strings.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/memory.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/math.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/limits.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+../../sdk/native/jni/include/opencv2/core/version.hpp:
+
+C:/Dev/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm:
+
+C:/Dev/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath:
+
+C:/Dev/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef:
+
+C:/Dev/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/complex:
+
+C:/Dev/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/map:
+
+C:/Dev/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/vector:
+
+../../sdk/native/jni/include/opencv2/core/operations.hpp:
+
+C:/Dev/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/limits:
+
+../../sdk/native/jni/include/opencv2/core/mat.hpp:
+
+../../sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp:
+
+../../sdk/native/jni/include/opencv2/objdetect/objdetect.hpp:
+
+C:/Dev/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/deque:
+
+C:/Dev/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/android/log.h:
---
+++ tutorial-1-camerapreview/src/org/opencv/samples/tutorial1/MainActivity.java
@@ -0,0 +1,162 @@
+package org.opencv.samples.tutorial1;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements CvCameraViewListener {
+    private static final String TAG = "Main Activity";
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private boolean mIsJavaCamera = true;
+    private MenuItem mItemSwitchCamera = null;
+    private Mat current;
+    private Mat previous;
+    private Mat difference;//difference between previous and current
+    private boolean first = true;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
+    public MainActivity() {
+        Log.i(TAG, "Instantiated new " + this.getClass());
+    }
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContentView(R.layout.tutorial1_surface_view);
+
+        if (mIsJavaCamera)
+            mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.tutorial1_activity_java_surface_view);
+        else
+            mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.tutorial1_activity_native_surface_view);
+
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+
+        mOpenCvCameraView.setCvCameraViewListener(this);
+        
+        LayoutInflater controlInflater = LayoutInflater.from(getBaseContext());
+        View viewControl = controlInflater.inflate(R.layout.controls, null);
+        LayoutParams layoutParamsControl
+         = new LayoutParams(LayoutParams.FILL_PARENT,
+         LayoutParams.FILL_PARENT);
+        this.addContentView(viewControl, layoutParamsControl);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_3, this, mLoaderCallback);
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    public void takePicture(View view) {
+    	//
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.i(TAG, "called onCreateOptionsMenu");
+        mItemSwitchCamera = menu.add("Toggle Native/Java camera");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String toastMesage = new String();
+        Log.i(TAG, "called onOptionsItemSelected; selected item: " + item);
+
+        if (item == mItemSwitchCamera) {
+            mOpenCvCameraView.setVisibility(SurfaceView.GONE);
+            mIsJavaCamera = !mIsJavaCamera;
+
+            if (mIsJavaCamera) {
+                mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.tutorial1_activity_java_surface_view);
+                toastMesage = "Java Camera";
+            } else {
+                mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.tutorial1_activity_native_surface_view);
+                toastMesage = "Native Camera";
+            }
+
+            mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+            mOpenCvCameraView.setCvCameraViewListener(this);
+            mOpenCvCameraView.enableView();
+            Toast toast = Toast.makeText(this, toastMesage, Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+        return true;
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+    	previous = new Mat(width, height, CvType.CV_64FC4);
+        current = new Mat(width, height, CvType.CV_64FC4);
+        difference = new Mat(width, height, CvType.CV_64FC4);
+    }
+
+    public void onCameraViewStopped() {
+    	current.release();
+    }
+
+    public Mat onCameraFrame(Mat inputFrame) {
+        inputFrame.copyTo(current);
+        if(first){//'first' is true only the first time the method is called
+            inputFrame.copyTo(previous);
+            first = false;
+            Log.i("First processing", " ");
+        }
+
+        Core.absdiff(current, previous, difference);
+    //  current.copyTo(previous);
+        inputFrame.copyTo(previous);
+
+        return difference;
+    }
+}
